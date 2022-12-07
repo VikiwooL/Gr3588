0b3ff35d4613518bdcc77aae5d796f7ac15d1d03
Viktiriya <viktoriya_202@mail.ru>
2022-12-07 21:51:09 +0300

    add solution for task 19,23

---
+++ Sem3Task0/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("введите пятизначное число:");
+string? num1Line = Console.ReadLine();
+int[] array = new int[5];
+if(num1Line != null)
+{
+    int num = int.Parse(num1Line);
+    int endIndex=5;
+    int startIndex=0;
+   while (startIndex<endIndex)
+    {
+        array[startIndex]=num%10;
+        num=num/10;
+        startIndex++;
+    }
+       if(array[0]==array[4] & array[1]==array[3])
+    {
+     Console.WriteLine("Да");
+    }
+    else
+    {
+       Console.WriteLine("нет"); 
+    }
+}
