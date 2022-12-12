b297254a175e17ca71ed93be92b29355c7ada520
Viktiriya <viktoriya_202@mail.ru>
2022-12-12 22:21:57 +0300

    add solution for task 27,29

---
+++ Sem4Task27/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("введите число:");
+string? num1Line = Console.ReadLine();
+if(num1Line != null)
+{
+    int num = int.Parse(num1Line);
+    int sum = 0;
+    while(num>0)
+    {
+        sum = sum + num%10;
+        num = num/10;
+    }
+        
+        Console.Write(sum);
+}
---
+++ Sem4Task29/Program.cs
@@ -0,0 +1,16 @@
+﻿void PrintArr(int[] arr)
+{
+    Console.Write("[");
+    for(int i = 0; i < arr.Length-1;i++)
+    {
+        Console.Write(arr[i]+", ");
+    }
+    Console.WriteLine(arr[arr.Length-1]+"]");
+}
+
+int[] array = new int[8];
+Random rand = new Random();
+for (int i = 0; i < array.Length; i++)
+   array[i] = rand.Next(0,101);
+
+PrintArr(array);
